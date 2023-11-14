66e7840609c6cf47e0d8ccbf9ab71c009d7d1c88
anwar.benihissa <anwar.benihissa@gmail.com>
2023-11-14 19:21:34 +0100

    my static web site

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My-static-website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
